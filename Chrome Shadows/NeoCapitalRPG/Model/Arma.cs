cce249a93a2513cbc026e5ca7fd314c4684454fb
Ana Júlia Leite <leite.anajulia25@gmail.com>
2025-09-24 19:05:27 -0300

    Atualização
    Separando as classes em uma pasta chamada Model e feita a primeira tentativa de rodar o jogo com o primeiro efeito sonoro estabelecido

---
+++ Chrome Shadows/NeoCapitalRPG/Model/Arma.cs
@@ -0,0 +1,23 @@
+﻿using NeoCapitalRPG.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeoCapitalRPG.Model
+{
+    public class Arma
+    {
+        public string Nome { get; set; }
+        public int Bonus { get; set; }
+        public string Descricao { get; set; }
+
+        public Arma(string nome, int bonus, string descricao)
+        {
+            Nome = nome;
+            Bonus = bonus;
+            Descricao = descricao;
+        }
+    }
+}
